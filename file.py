d8e6b98e46a6e40424f77985d13f1fed56731dd1
Danish <quazidanish.work@gmail.com>
2023-12-29 14:07:07 +0000

    adding a new file

---
+++ file.py
@@ -0,0 +1,13 @@
+# Specify the file path in the D drive where you want to create the file
+file_path = "D:/one.txt"
+
+# Open the file in write mode ('w') to create it
+# You can also use 'a' mode to append to an existing file
+try:
+    with open(file_path, 'w') as file:
+        # You can write content to the file if needed
+        file.write("This is a sample text in the file.")
+    print(f"File '{file_path}' created successfully.")
+except Exception as e:
+    print(f"Error: {str(e)}")
+
